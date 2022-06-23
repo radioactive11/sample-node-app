df89024e59f376b15c90ac52c4a31b0b709c2ddb
Arijit Roy <roy.arijit2001@gmail.com>
2022-06-24 01:18:15 +0530

    changed port to 8000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
-const port = 5000;
+const port = 8000;
 
 app.get("/", (req, res) => {
   res.send("lol");
